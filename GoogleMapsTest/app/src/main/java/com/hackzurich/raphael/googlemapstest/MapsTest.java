4ae2ab8aeb6abe925e61f61702460236f4518d18
Hantao <zhthantao@users.noreply.github.com>
2017-09-16 16:24:28 +0200

    Update
    Add Google utility library and demo
    Temporary pop up window with reward info

--- GoogleMapsTest/app/src/main/java/com/hackzurich/raphael/googlemapstest/MapsTest.java
+++ GoogleMapsTest/app/src/main/java/com/hackzurich/raphael/googlemapstest/MapsTest.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.GeoApiContext;
+import com.google.maps.android.ui.IconGenerator;
 import com.google.maps.model.TravelMode;
 import com.google.maps.DirectionsApi;
 import com.google.maps.model.DirectionsResult;
@@ -40,7 +41,7 @@ public class MapsTest extends FragmentActivity implements OnMapReadyCallback,
 
     private static final int PATTERN_GAP_LENGTH_PX = 50;
     private static final int PATTERN_DASH_LENGTH_PX = 20;
-
+    private static Polyline[] polylines = new Polyline[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,13 +75,12 @@ public class MapsTest extends FragmentActivity implements OnMapReadyCallback,
             DirectionsResult resultPT = DirectionsApi.newRequest(getGeoContext()).alternatives(true).mode(TravelMode.TRANSIT).origin(origin).destination(destination).departureTime(now).await();
             //addPolylines(resultPT, mMap);
             addCustomizedPolyline(resultPT, mMap,"Bike", Color.BLACK);
-            addCustomizedPolyline(resultPT, mMap,"Bus", Color.BLUE);
             addCustomizedPolyline(resultPT, mMap,"Walk", Color.RED);
+            addCustomizedPolyline(resultPT, mMap,"Bus", Color.BLUE);
 
-            addMarkersToMap(resultPT,mMap, 0, "1000", 0.5);
-            addMarkersToMap(resultPT,mMap, 1, "500", 0.6);
-            addMarkersToMap(resultPT,mMap, 2, "200", 0.4);
-
+            addMarkersToMapNew(resultPT,mMap, 0, "200", 0.5);
+            addMarkersToMapNew(resultPT,mMap, 1, "1000", 0.5);
+            addMarkersToMapNew(resultPT,mMap, 2, "0", 0.1);
 
             List<LatLng> decodedPath = PolyUtil.decode(resultPT.routes[0].overviewPolyline.getEncodedPath());
 
@@ -120,6 +120,23 @@ public class MapsTest extends FragmentActivity implements OnMapReadyCallback,
         marker.setInfoWindowAnchor(.5f,1.0f);
     }
 
+    private void addMarkersToMapNew(DirectionsResult results, GoogleMap mMap,int index,String reward, double percentage) {
+        LatLng middlePoint = computeCentroid(PolyUtil.decode(results.routes[index].overviewPolyline.getEncodedPath()), percentage);
+//        Marker marker = mMap.addMarker(new MarkerOptions()
+//                .position(middlePoint)
+//                .title(results.routes[0].legs[0].startAddress)
+//                .visible(true)
+//                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN))
+//                .title("Reward")
+//                .snippet(reward));
+//        marker.showInfoWindow();
+//        marker.setInfoWindowAnchor(.5f,1.0f);
+
+        IconGenerator iconFactory = new IconGenerator(this);
+        iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
+        addIcon(iconFactory, reward, middlePoint);
+    }
+
     private String getEndLocationTitle(DirectionsResult results){
         return  "Time :"+ results.routes[0].legs[0].duration.humanReadable + " Distance :" + results.routes[0].legs[0].distance.humanReadable;
     }
@@ -128,33 +145,33 @@ public class MapsTest extends FragmentActivity implements OnMapReadyCallback,
     switch (trafficType){
         case "Bike" :
             List<LatLng> decodedPath = PolyUtil.decode(results.routes[0].overviewPolyline.getEncodedPath());
-            Polyline polyline1 = mMap.addPolyline(new PolylineOptions()
+            polylines[0] = mMap.addPolyline(new PolylineOptions()
                     .clickable(true)
                     .addAll(decodedPath)
                     .width(15)
                     .color(routeColor));
-            polyline1.setTag("Bike");
-            polyline1.setPattern(PATTERN_POLYLINE_DOTTED);
+            polylines[0].setTag("Bike");
+            polylines[0].setPattern(PATTERN_POLYLINE_DOTTED);
             break;
         case "Walk" :
             List<LatLng> decodedPath2 = PolyUtil.decode(results.routes[1].overviewPolyline.getEncodedPath());
-            Polyline polyline2 = mMap.addPolyline(new PolylineOptions()
+            polylines[1] = mMap.addPolyline(new PolylineOptions()
                     .clickable(true)
                     .addAll(decodedPath2)
                     .width(15)
-                    .color(Color.GREEN));
-            polyline2.setTag("Walk");
-            polyline2.setPattern(PATTERN_POLYLINE_DASHED);
+                    .color(routeColor));
+            polylines[1].setTag("Walk");
+            polylines[1].setPattern(PATTERN_POLYLINE_DASHED);
             break;
         case "Bus" :
             List<LatLng> decodedPath3 = PolyUtil.decode(results.routes[2].overviewPolyline.getEncodedPath());
-            Polyline polyline3 = mMap.addPolyline(new PolylineOptions()
+            polylines[2] = mMap.addPolyline(new PolylineOptions()
                     .clickable(true)
                     .addAll(decodedPath3)
                     .width(15)
-                    .color(Color.RED));
-            polyline3.setTag("Bus");
-            polyline3.setPattern(PATTERN_POLYLINE_GAPED);
+                    .color(routeColor));
+            polylines[2].setTag("Bus");
+            polylines[2].setPattern(PATTERN_POLYLINE_GAPED);
             break;
     }
     }
@@ -186,27 +203,51 @@ public class MapsTest extends FragmentActivity implements OnMapReadyCallback,
 //            polyline.setPattern(null);
 //        }
 
+
+        int reward = 0;
+        int time = 0;
+
+        switch (polyline.getTag().toString()) {
+            case "Bike":
+                reward = 200;
+                time = 15;
+                break;
+            case "Bus":
+                reward = 0;
+                time = 10;
+                break;
+            case "Walk":
+                reward = 1000;
+                time = 25;
+                break;
+        }
         if (polyline.getWidth() < 20) {
+            for (Polyline polylineX : polylines) {
+                polylineX.setWidth(15);
+            }
             polyline.setWidth(50);
+            Toast.makeText(this,"Choose to " + polyline.getTag().toString() + ", it will take " + time + "mins, and get reward of " +  Integer.toString(reward),
+                    Toast.LENGTH_SHORT).show();
         } else {
             // The default pattern is a solid stroke.
             polyline.setWidth(15);
+
         }
 
-        Toast.makeText(this, polyline.getTag().toString(),
-                Toast.LENGTH_SHORT).show();
     }
 
 
     private LatLng computeCentroid(List<LatLng> points, double percentage) {
-        double latitude = 0;
-        double longitude = 0;
         int n = points.size();
-
-        for (LatLng point : points) {
-            latitude += point.latitude;
-            longitude += point.longitude;
-        }
         return new LatLng(points.get((int)(percentage*n)).latitude,points.get((int)(percentage*n)).longitude);
+    }
+
+    private void addIcon(IconGenerator iconFactory, CharSequence text, LatLng position) {
+        MarkerOptions markerOptions = new MarkerOptions().
+                icon(BitmapDescriptorFactory.fromBitmap(iconFactory.makeIcon(text))).
+                position(position).
+                anchor(iconFactory.getAnchorU(), iconFactory.getAnchorV());
+
+        mMap.addMarker(markerOptions);
     }
 }
